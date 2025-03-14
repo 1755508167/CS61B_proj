0418358cba88ba5799765c2d57d3c004cb4ffc57
Han <1755508167@qq.com>
2025-03-14 15:13:17 +0800

    have done task3

---
+++ proj1b/Palindrome.java
@@ -0,0 +1,34 @@
+public class Palindrome {
+    /*
+    给定一个String，把这个字符串拆成一个个字符，然后放进一个队列里。比如把"apple"拆成'a','p','p'...
+    */
+    public Deque<Character> wordToDeque(String word) {
+        int length = word.length();
+        LinkedListDeque<Character> listDeque=new LinkedListDeque<>();
+        for (int i = 0;i < length;i++ ){
+            listDeque.addLast(word.charAt(i));
+        }
+        return listDeque;
+    }
+    //如果给定的word是Palindrome(回文)，返回true，否则返回false
+    public boolean isPalindrome(String word) {
+        Palindrome p=new Palindrome();
+        Deque<Character> deque=p.wordToDeque(word);
+        int j=deque.size()-1;
+        for(int i=0;i<deque.size();i++){
+            if(deque.get(i) != deque.get(j)){
+                return false;
+            }else {
+                j--;
+                continue;
+            }
+        }
+        return true;
+    }
+
+    public static void main(String[] args){
+        Palindrome p=new Palindrome();
+        Deque<Character> d=p.wordToDeque("hello");
+        d.printDeque();
+    }
+}
--- proj1b/TestPalindrome.java
+++ proj1b/TestPalindrome.java
@@ -2,7 +2,7 @@ import org.junit.Test;
 import static org.junit.Assert.*;
 
 public class TestPalindrome {
-    /*// You must use this palindrome, and not instantiate
+    // You must use this palindrome, and not instantiate
     // new Palindromes, or the autograder might be upset.
     static Palindrome palindrome = new Palindrome();
 
@@ -14,5 +14,13 @@ public class TestPalindrome {
             actual += d.removeFirst();
         }
         assertEquals("persiflage", actual);
-    } Uncomment this class once you've created your Palindrome class. */
+    } //Uncomment this class once you've created your Palindrome class.
+
+    @Test
+    public void isPalindrome() {
+        Palindrome p=new Palindrome();
+        Deque<Character> deque=p.wordToDeque("lol");
+        assertTrue(palindrome.isPalindrome("lol"));
+        assertFalse(palindrome.isPalindrome("cat"));
+    }
 }
