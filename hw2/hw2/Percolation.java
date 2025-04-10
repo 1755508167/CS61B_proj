fd95945222c34b5241d871e0cd810fb13b44ac5a
Han <1755508167@qq.com>
2025-04-10 08:59:10 +0800

    first upload

--- hw2/hw2/Percolation.java
+++ hw2/hw2/Percolation.java
@@ -3,5 +3,118 @@ package hw2;
 import edu.princeton.cs.algs4.WeightedQuickUnionUF;
 
 public class Percolation {
+    private int size;
+    private boolean[][] system;
+    private WeightedQuickUnionUF uf;
+    //这是两个虚拟节点
+    private int virtualTop;//这个节点与第一行的所有节点相连
+    private int virtualBottom;//这个节点与最后一行的所有节点相连
+    //系统中处于open状态的数量
+    private int count=0;
 
+    // create N-by-N grid, with all sites initially blocked
+    public Percolation(int N){
+        if (N <= 0){
+            throw new java.lang.IndexOutOfBoundsException("Num should not be negetive");
+        }
+
+        size=N;
+        int totalSites=N*N;
+        uf=new WeightedQuickUnionUF(totalSites+2);//多两个虚拟节点
+        virtualTop = totalSites;
+        virtualBottom = totalSites + 1;
+        //默认值就是flase，无需再初始化
+        system=new boolean[size][size];
+    }
+    // open the site (row, col) if it is not open already
+    public void open(int row, int col) {
+        if (!system[row][col]) {
+            system[row][col] = true;
+        } else {
+            if ((row < 0 || row > size) && (col < 0 || col > size)) {
+                throw new java.lang.IndexOutOfBoundsException("index is out of range");
+            }
+            system[row][col] = true;
+            count++;
+            //转换坐标
+            int index = getIndex(row, col);
+            //如果打开的是第一行的节点，那么把这个节点连接到虚拟顶部
+            if (row == 0) {
+                uf.union(index, virtualTop);
+            }
+            //如果打开的是最后一行的节点，那么把这个节点连接到虚拟顶部
+            if (row == size - 1) {
+                uf.union(index, virtualBottom);
+            }
+            //然后合并此节点上下左右中处于open状态的节点
+            int[][] directions = {{-1, 0}, {1, 0}, {0, -1}, {0, 1}};
+            for (int[] dir : directions) {
+                int newRow = row + dir[0];
+                int newCol = col + dir[1];
+                if (validate(newRow, newCol) && isOpen(newRow, newCol)) {
+                    int neighbroIndex = getIndex(newRow, newCol);
+                    uf.union(neighbroIndex, index);
+                }
+            }
+        }
+    }
+    // is the site (row, col) open?
+    public boolean isOpen(int row, int col){
+        if (validate(row,col)){
+            return system[row][col]==true;
+        }else {
+            throw new IndexOutOfBoundsException("Invalid row or column index");
+        }
+
+    }
+    //判断一个节点是否与顶部相连
+    public boolean isFull(int row, int col){
+        if (validate(row,col)){
+            if (!isOpen(row,col)){
+                return false;
+            }else {
+                return uf.connected(getIndex(row,col),virtualTop);
+            }
+        }else {
+            throw new IndexOutOfBoundsException("Invalid row or column index");
+        }
+
+    }  // is the site (row, col) full?
+    // number of open sites
+    public int numberOfOpenSites(){
+        return count;
+    }
+
+    // does the system percolate?
+    public boolean percolates(){
+        return uf.connected(virtualTop,virtualBottom);
+    }
+    //确保行列坐标在有效范围内
+    private boolean validate(int row,int col){
+        if (row < 0 || row >= size || col < 0 || col >= size) {
+            return false;
+            //throw new IndexOutOfBoundsException("Invalid row or column index");
+        }else {
+            return true;
+        }
+    }
+
+    //把二维坐标转化为一维的索引
+    private int getIndex(int row,int col){
+        return row*size+col;
+    }
+
+    public static void main(String[] args){
+        Percolation p=new Percolation(5);
+        p.open(4,4);
+        //初始化一个并查集
+        WeightedQuickUnionUF set=new WeightedQuickUnionUF(5);
+        //返回4所在的集合的代表元素
+        System.out.println(set.find(4));
+        System.out.println(set.count());
+        set.union(1,2);
+        System.out.println(set.connected(1,2));
+        System.out.println(set.find(2));
+
+    }   // use for unit testing (not required)
 }
--- hw2/hw2/PercolationStats.java
+++ hw2/hw2/PercolationStats.java
@@ -1,5 +1,76 @@
 package hw2;
+import edu.princeton.cs.introcs.StdRandom;
+
+import java.util.ArrayList;
 
 public class PercolationStats {
+    private ArrayList<Double> threshold;
+    private double rate;
+    // perform T independent experiments on an N-by-N grid
+    //执行T次独立的计算
+    public PercolationStats(int N, int T, PercolationFactory pf){
+        if (N <= 0 || T <= 0){
+            throw new java.lang.IllegalArgumentException("N and T should not be negetive");
+        }
+        threshold=new ArrayList<>();
+
+        for (int i =0;i<T;i++){
+            Percolation p=pf.make(N);
+            while (!p.percolates()){
+                //随机生成坐标
+                int randomRow=StdRandom.uniform(N);
+                int randomCol=StdRandom.uniform(N);
+
+                //打开这个节点
+                p.open(randomRow,randomCol);
+            }
+            int openNums=p.numberOfOpenSites();
+            //System.out.println("openNums:"+openNums);
+            rate=(openNums*1.0)/(N*N);
+            threshold.add(rate);
+
+            System.out.println(rate);
+
+        }
+
+
+    }
+    // sample mean of percolation threshold
+    //计算渗透率的平均值
+    public double mean(){
+        double sum=0.0;
+        for (double r : threshold){
+            sum+=r;
+        }
+        return sum/threshold.size();
+    }
+    // sample standard deviation of percolation threshold
+    //样本标准差
+    public double stddev(){
+        double mean = mean();
+        double sum = 0.0;
+        for (double r : threshold) {
+            sum += (r - mean) * (r - mean);
+        }
+        return Math.sqrt(sum / (threshold.size() - 1));
+    }
+    // low endpoint of 95% confidence interval
+    //置信区间的下界
+    public double confidenceLow(){
+        return mean() - (1.96 * stddev() / Math.sqrt(threshold.size()));
+    }
+    // high endpoint of 95% confidence interval
+    //置信区间的上界
+    public double confidenceHigh(){
+        return mean() + (1.96 * stddev() / Math.sqrt(threshold.size()));
+    }
+
+    public static void main(String[] args){
+        PercolationFactory pf=new PercolationFactory();
+        PercolationStats p=new PercolationStats(20,70,pf);
+        System.out.println("mean = " + p.mean());
+        System.out.println("stddev = " + p.stddev());
+        System.out.println("95% confidence interval = [" + p.confidenceLow() + ", " + p.confidenceHigh() + "]");
+    }
 
 }
