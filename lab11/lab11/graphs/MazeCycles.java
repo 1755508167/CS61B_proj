4b50215c83cfe94be6a8dcb166c3f336a44ab730
Han <1755508167@qq.com>
2025-05-19 19:28:42 +0800

    second upload:have finished BFS and cycle detect

--- lab11/lab11/graphs/MazeCycles.java
+++ lab11/lab11/graphs/MazeCycles.java
@@ -1,7 +1,7 @@
 package lab11.graphs;
 
 /**
- *  @author Josh Hug
+ * @author Josh Hug
  */
 public class MazeCycles extends MazeExplorer {
     /* Inherits public fields:
@@ -9,16 +9,60 @@ public class MazeCycles extends MazeExplorer {
     public int[] edgeTo;
     public boolean[] marked;
     */
+    private boolean hasCycle = false;
+    private int[] parent;
 
     public MazeCycles(Maze m) {
         super(m);
+        maze = m;
+        parent=new int[m.V()];
     }
 
+    //环路检测算法的实现
     @Override
     public void solve() {
         // TODO: Your code here!
+        for (int v = 0; v < maze.V(); v++) {
+            if (!marked[v]) {
+                dfs(v, -1);
+                if (hasCycle) return;
+            }
+        }
     }
 
+    //深度优先搜索
+    private void dfs(int v,int from) {
+        marked[v]=true;
+        announce();
+
+        for (int w:maze.adj(v)){
+            if (hasCycle){
+                return;
+            }
+            if (!marked[w]){
+                edgeTo[w]=v;
+                parent[w]=v;
+                announce();
+                dfs(w,v);
+            } else if (w != from) {
+                edgeTo[w]=v;
+                reconstructCycle(v, w);
+                hasCycle = true;
+                return;
+            }
+        }
+    }
     // Helper methods go here
+
+    private void reconstructCycle(int v,int w){
+        //回溯路径
+        int x=v;
+        while (x != w){
+            edgeTo[w]=parent[x];
+            x=parent[x];
+        }
+        edgeTo[w] = x;
+        announce();  // Show the cycle
+    }
 }
 
